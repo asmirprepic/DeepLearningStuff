884fbcdbb88adbc0ec352e8d1fecd09e1e781c2f
asmirp <asmirprepic@gmail.com>
2024-08-12 22:31:46 +0200

    Added transformer_block

---
+++ Classes/multihead_attention.py
@@ -0,0 +1,6 @@
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, LayerNormalization  # Import necessary Keras layers
+from tensorflow.keras import Sequential 
+
+
---
+++ Classes/transformer_block.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, LayerNormalization  # Import necessary Keras layers
+from tensorflow.keras import Sequential 
+
+class TransformerBlock(tf.keras.layers.Layer):
+    def __init__(self,units,num_heads,ff_dim):
+        super(TransformerBlock,self).__init__()
+        self.attention = MultiHeadAttention(num_heads,units)
+        self.ffn = tf.keras.Sequential([
+            Dense(ff_dim,activation = 'relu')
+            Dense(units)
+        ])
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon = 1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon = 1e-6)
+
+    def call(self,inputs):
+        attn_output = self.attention(inputs)
+        attn_output = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(attn_output)
+        return self.layernorm2(attn_output+ ffn_output)
