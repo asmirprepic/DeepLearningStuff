af3044957f7345d6df82179e3da4eee4f956e613
asmirp <asmirprepic@gmail.com>
2024-08-19 23:54:44 +0200

    Added lstm model class

---
+++ Classes/lstm_model.py
@@ -0,0 +1,27 @@
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+classLSTMModel(tf.keras.Model):
+    def__init__(self, units, num_outputs):
+        super(LSTMModel, self).__init__()
+        # Define layers
+        self.lstm1 = layers.LSTM(units, return_sequences=True)
+        self.lstm2 = layers.LSTM(units)
+        self.dense = layers.Dense(128, activation='relu')
+        self.output_layer = layers.Dense(num_outputs)
+
+    defcall(self, inputs, training=False):
+        x = self.lstm1(inputs)
+        x = self.lstm2(x)
+        x = self.dense(x)
+        return self.output_layer(x)
+
+# Example usage
+units = 128
+num_outputs = 1# For single-output regression
+
+model = LSTMModel(units=units, num_outputs=num_outputs)
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+model.build(input_shape=(None, 100, 64))
+model.summary()
