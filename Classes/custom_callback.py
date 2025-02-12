a020b7377d80a749d509bb57203835c1fc7e4bf1
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2025-02-12 23:59:57 +0100

    Update custom_callback.py

--- Classes/custom_callback.py
+++ Classes/custom_callback.py
@@ -1,5 +1,7 @@
 from tensorflow.keras.callbacks import Callback
 
 class CustomCallback(Callback):
+
+    
     def on_epoch_end(self,epoch,logs = None):
         print(f"Epoch {epoch +1}: Custom callback log message")
