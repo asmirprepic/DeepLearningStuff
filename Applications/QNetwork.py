babff114990110a1366284292fcd58d94cb6e5b4
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2025-02-27 23:59:36 +0100

    Create QNetwork.py

---
+++ Applications/QNetwork.py
@@ -0,0 +1,13 @@
+class QNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=128):
+        super(QNetwork, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, action_dim)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
