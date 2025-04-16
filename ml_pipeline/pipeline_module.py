34f571d44a614385a50698798b57382c67f92da3
asmirprepic <39712457+asmirprepic@users.noreply.github.com>
2025-04-17 00:10:05 +0200

    Update pipeline_module.py

--- ml_pipeline/pipeline_module.py
+++ ml_pipeline/pipeline_module.py
@@ -21,4 +21,41 @@ def create_pipeline(numeric_features, categorical_features):
     ('imputer',SimpleImputer(strategy = 'most_frequent')),
     ('encoder', OneHotEncoder(handle_unkown = 'ignore'))
   ])
-  
+
+  preprocessor = ColumnTransformer(
+    transformers = [
+      ('num',numeric_transformers,numeric_features),
+      ('cat',categorical_transformer,categorical_features)
+    ]
+  )
+  model_pipeline = Pipeline(steps = [
+    ('preprocessor',preprocessor),
+    ('classifier', RandomForestClassifier(n_estimators = 100, random_state = 123))
+  ]
+  )
+  return model_pipeline
+
+def train_and_save(data: pd.DataFrame,traget_col: str, model_path: str): 
+  y = data[target_col]
+  X = data.drop(columns = [target_col])
+
+  numeric_features = X.select_dtypes(include = ['int64,float64']).columns.tolist()
+  categorical_features = X.select_dtypes(include = ['object','category']).columns.tolist()
+
+  X_tran,X_test, y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+  pipeline = create_pipeline(numeric_features,categorical_features)
+  pipeline.Fit(X_train,y_train)
+
+  os.makedirs(os.path.dirname(model_path),exist_ok = True)
+  joblib.dump(pipeline,model_path)
+
+  print(f"Model pipeline saved to {model_path}")
+
+
+def load_model(model_path:str):
+  return joblib.load(model_path)
+
+def predict(model,input_data: pd.DataFrame):
+  return model.predict(input_data)
+
+
